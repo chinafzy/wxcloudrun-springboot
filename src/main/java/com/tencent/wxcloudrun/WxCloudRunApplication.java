b657de65869369d54b2503d1b87d47e1cee016a0
fangzeyu <zeyu.fang@chelaile.net.cn>
2023-02-20 21:47:51 +0800

    jj

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WxCloudRunApplication {  
 
   public static void main(String[] args) {
+    System.out.println("here i am");
     SpringApplication.run(WxCloudRunApplication.class, args);
   }
 }
